9c523a1093b8322f94a96f51b763d99bab22cdf1
Jan Potoms <potoms.jan@gmail.com>
2016-07-31 21:28:48 +0200

    don't do offset/limit

--- README.md
+++ README.md
@@ -11,57 +11,51 @@ It can process them in parallel.
 
 ```js
 var processBatches = require('process-batches');
+var BATCH_SIZE = 100;
 
-processBatches((offset, limit) => {
+processBatches(i => {
+  var offset = i * BATCH_SIZE;
+  var limit = BATCH_SIZE;
   return db.getUsers(offset, limit);
 }, {
-  size: 10,
   concurrency: 5
 }, users => {
   console.log(`sending email to ${users.length} users`);
   return Promise.all(users.map(user => {
-    return sendEmail(user.email);
+    return mailSvc.sendEmail(user.email);
   }));
 })
   .then(() => {
-    console.log('done sending emails')
+    console.log('done sending emails');
   });
 ```
 
 ## API
 
 ```js
-processBatches(source, options, processFn)
+processBatches(fetchFn, [options], processFn)
 ```
 
 Returns a promise that is fulfilled when all the batches are processed.
 
-### `source`
+### `fetchFn`
 
 ```
-Function(number offset, number limit) -> Promise<Array>|Array
+Function(number index) -> Promise<Array>|Array
 ```
 
-```
-Array
-```
-
-Function that takes two parameters, `offset` and `limit`.
-It is used to fetch the batches. It can either return an array or a promise.
+Function that takes one parameters, the `index` of the batch.
+It is used to fetch the batches. It can either return an array or a promise for an array.
 To signal the end, either return `null` or an empty array.
-Alternatively this parameter can also be just a array which will be sliced to form the batches.
 
 ### `options`
 
-```
-Object {
-  number size,
-  number concurrency = 1
-}
-```
+This parameter is optional.
 
 ```
-number
+Object {
+  number concurrency = 1
+}
 ```
 
 Configuration for processing. `size` determines the batch sizes.
@@ -71,8 +65,8 @@ Alternatively you can also just pass the batch size as a number.
 ### `processFn`
 
 ```
-Function(Array batch, number offset) processFn -> Promise
+Function(Array batch, number index) processFn -> Promise
 ```
 
-The function that will process the batch. It is passed the current `batch` and `offset`.
+The function that will process the batch. It is passed the current `batch` and `index` of the batch.
 If you return a promise, the module will wait until the promise is fulfilled before starting the next batch.
--- test/batchProcess.spec.js
+++ test/batchProcess.spec.js
@@ -7,43 +7,33 @@ function delay (time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
-describe('processBatches', function () {
-  it('maps arrays in batches', function () {
-    var batches = [];
-    return processBatches([1, 2, 3, 4, 5, 6], 2, batch => batches.push(batch))
-      .then(() => assert.deepEqual(batches, [[1, 2], [3, 4], [5, 6]]));
-  });
+function fromArray (array, batchSize = 1) {
+  return i => {
+    var fromIndex = i * batchSize;
+    var toIndex = fromIndex + batchSize;
+    return array.slice(fromIndex, toIndex);
+  };
+}
 
-  it('maps empty array', function () {
-    return processBatches([], 2, batch => {
+describe('processBatches', function () {
+  it('maps empty iterator', function () {
+    return processBatches(() => null, 2, batch => {
       throw new Error('shouldn\'t be called');
     });
   });
 
-  it('passes offset', function () {
-    var offsets = [];
-    return processBatches([1, 2, 3, 4, 5], 2, (batch, offset) => {
-      offsets.push(offset);
+  it('passes index', function () {
+    var indices = [];
+    return processBatches(fromArray([1, 2, 3, 4, 5], 2), (batch, offset) => {
+      indices.push(offset);
     })
-      .then(() => assert.deepEqual(offsets, [0, 2, 4]));
-  });
-
-  it('larger batch size than source array', function () {
-    var batches = [];
-    return processBatches([1, 2, 3], 4, batch => batches.push(batch))
-      .then(() => assert.deepEqual(batches, [[1, 2, 3]]));
-  });
-
-  it('cuts off last batch', function () {
-    var batches = [];
-    return processBatches([1, 2, 3, 4, 5], 3, batch => batches.push(batch))
-      .then(() => assert.deepEqual(batches, [[1, 2, 3], [4, 5]]));
+      .then(() => assert.deepEqual(indices, [0, 1, 2]));
   });
 
   it('does promises', function () {
     var batches = [];
     var counter = 0;
-    return processBatches([1, 2, 3, 4, 5, 6], 2, batch => {
+    return processBatches(fromArray([1, 2, 3, 4, 5, 6], 2), batch => {
       assert.strictEqual(counter, 0);
       batches.push(batch);
       counter += 1;
@@ -60,7 +50,7 @@ describe('processBatches', function () {
   it('fails on error', function () {
     var batches = [];
     var error = new Error('test');
-    return processBatches([1, 2], 1, batch => {
+    return processBatches(fromArray([1, 2], 1), batch => {
       batches.push(batch);
       return Promise.reject(error);
     })
@@ -75,7 +65,7 @@ describe('processBatches', function () {
   it('fails on synchronous error', function () {
     var batches = [];
     var error = new Error('test');
-    return processBatches([1, 2], 1, batch => {
+    return processBatches(fromArray([1, 2], 1), batch => {
       batches.push(batch);
       throw error;
     })
@@ -90,7 +80,7 @@ describe('processBatches', function () {
   it('can use a fetch function', function () {
     var batches = [];
     var input = [[1, 2], [3, 4, 5], []];
-    return processBatches((offset, limit) => {
+    return processBatches(() => {
       return input.shift();
     }, 2, batch => {
       batches.push(batch);
@@ -103,7 +93,7 @@ describe('processBatches', function () {
   it('can use an async fetch function', function () {
     var batches = [];
     var input = [[1, 2], [3, 4, 5], []];
-    return processBatches((offset, limit) => {
+    return processBatches(() => {
       return Promise.resolve(input.shift());
     }, 2, batch => {
       batches.push(batch);
@@ -115,7 +105,7 @@ describe('processBatches', function () {
 
   it('handles exceptions in the fetch function', function () {
     var error = new Error('test');
-    return processBatches((offset, limit) => {
+    return processBatches(() => {
       throw error;
     }, 2, batch => {
       throw new Error('not supposed to be called');
@@ -127,7 +117,7 @@ describe('processBatches', function () {
 
   it('handles rejections in the fetch function', function () {
     var error = new Error('test');
-    return processBatches((offset, limit) => {
+    return processBatches(() => {
       return Promise.reject(error);
     }, 2, batch => {
       throw new Error('not supposed to be called');
@@ -140,7 +130,7 @@ describe('processBatches', function () {
   it('can end the sequence on null', function () {
     var batches = [];
     var input = [[1, 2], [3, 4, 5], null];
-    return processBatches((offset, limit) => {
+    return processBatches(() => {
       return Promise.resolve(input.shift());
     }, 2, batch => {
       batches.push(batch);
@@ -152,18 +142,17 @@ describe('processBatches', function () {
 
   it('can process concurrently', function () {
     var batches = [];
-    var offsets = [];
+    var indices = [];
     var counter = 0;
     var delays = [50, 100, 70, 30, 30];
     var counts = [];
-    return processBatches([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], {
-      size: 2,
+    return processBatches(fromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2), {
       concurrency: 3
     }, (batch, offset) => {
       counter += 1;
       counts.push(counter);
       batches.push(batch);
-      offsets.push(offset);
+      indices.push(offset);
       return delay(delays.shift())
         .then(() => {
           counter -= 1;
@@ -171,7 +160,7 @@ describe('processBatches', function () {
     })
       .then(() => {
         assert.deepEqual(batches, [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]);
-        assert.deepEqual(offsets, [0, 2, 4, 6, 8]);
+        assert.deepEqual(indices, [0, 1, 2, 3, 4]);
         assert.deepEqual(counts, [1, 2, 3, 3, 3]);
       });
   });
